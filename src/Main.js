5c45e4969b02feb287c3c83929b5aae06510a8c8
Waruwat Mai <61070214@kmitl.ac.th>
2018-12-24 09:48:27 +0700

    first commit

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react'
+
+class Main extends Component {
+    constructor() {
+
+    }
+    render() {
+        return(
+            <div>
+                asdasd
+            </div>
+        );
+    }
+}
